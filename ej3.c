5d4c9dce40d1cbddb1e742dd81878fa5e93b25b8
Maxi Huebra <104871583+MaxiHbra@users.noreply.github.com>
2024-10-30 00:01:08 -0300

    Calcular resto recursivo.
    Dada la siguiente definición recursiva:
    Formule el módulo recursivo y la traza para el cálculo del resto (22,5).
    Nota: Esta función permite encontrar el resto de la división entera entre X e Y.

---
+++ ej3.c
@@ -0,0 +1,20 @@
+/*
+Dada la siguiente definición recursiva:
+Formule el módulo recursivo y la traza para el cálculo del resto (22,5).
+Nota: Esta función permite encontrar el resto de la división entera entre X e Y.*/
+#include <stdio.h>
+int resto_rec(int, int);
+int main() {
+	int x,x2;
+	printf("\nIngresar numero: ");
+	scanf("%d",&x);
+	printf("\nIngresar divisor: ");
+	scanf("%d",&x2);
+	printf("\n El resto de %d / %d es = %d",x,x2,resto_rec(x,x2));
+	return 0;
+}
+int resto_rec(int num, int div){
+	if(div>num) return num;
+	else return resto_rec(num-div,div);
+}
+
