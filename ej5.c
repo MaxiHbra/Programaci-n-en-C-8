ceb6de88d941637edb8940a9dae5027b595dd5b5
Maxi Huebra <104871583+MaxiHbra@users.noreply.github.com>
2024-11-01 10:40:08 -0300

    Mostrar digitos recursivo.
    Ejercicio 5: Mostrar los dígitos de un número natural X de derecha a izquierda. Realizar la traza para X=
    45673. Modificar el módulo para que muestre los dígitos de izquierda a derecha. (¿El módulo recursivo
    lleva return?)

---
+++ ej5.c
@@ -0,0 +1,25 @@
+/*Ejercicio 5: Mostrar los dígitos de un número natural X de derecha a izquierda. Realizar la traza para X=
+45673. Modificar el módulo para que muestre los dígitos de izquierda a derecha. (¿El módulo recursivo
+lleva return?).
+*/
+#include <stdio.h>
+void mostrar_dig_rec(int);
+int main() {
+int p;
+printf("\nIngresar numero: ");
+scanf("%d",&p);
+mostrar_dig_rec(p);
+return 0;
+}
+void mostrar_dig_rec(int num){ /*derecha a izquierda*/
+if(num!=0){
+printf("\n%d",num%10);
+mostrar_dig_rec(num/10);
+}
+}
+/*void mostrar_dig_rec(int num){ izquierda a derecha
+if(num!=0){
+mostrar_dig_rec(num/10);
+printf("\n%d",num%10);
+}
+}*/
